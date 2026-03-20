adcde5079730c4e0e36c6d18c4212fc79a39f10f
ginaklimmy <klimchockr@vcu.edu>
2026-03-19 20:27:26 -0400

    jqueryyyyy ayeeee

---
+++ jquery/main.js
@@ -0,0 +1,43 @@
+$(".dark-btn").click(
+    function(){
+  $(".box").toggleClass("dark");
+  $(".container").toggleClass("dark");
+}
+);
+
+$(".spin-btn").click(
+    function(){
+  $(".box").toggleClass("spin");
+}
+);
+
+$(".reveal-btn").click(
+    function(){
+
+        $(".pickles").show();
+        $(".reveal-btn").hide();
+
+        // $(".pickles").css("display","block")
+
+        // $(".pickles").addClass("reveal");
+    }
+);
+
+// $(".pickles").draggable();
+// $(".box").draggable();
+
+
+$(".draggable").draggable({
+    containment:".container", 
+    scroll:false
+});
+
+
+
+// $("button").click(
+//     funtion(){
+//         $(".box").toggleClass("spin");
+//     }
+// );
+
+// document.querySelector(".box").classList.toggle("main");
